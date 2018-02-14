74ff69bc256ed75011780af6d3543fea266efc01
Craig Morris <cr@igmorr.is>
2018-02-14 15:20:25 +0000

    rejig networks

--- truffle.js
+++ truffle.js
@@ -2,18 +2,24 @@ const ganache = require("ganache-cli")
 
 module.exports = {
   networks: {
-    development: {
+    ganache: {
+      host: "localhost",
+      port: 7545,
+      network_id: "*",
+      gas: 4600000
+    },
+    'ganache-cli': {
+      host: "localhost",
+      port: 8545,
+      network_id: "*",
+      gas: 4600000
+    },
+    console: {
       host: "localhost",
       port: 9545,
       network_id: "*",
       gas: 4600000
     },
-    // test: {
-    //   host: "localhost",
-    //   port: 7545,
-    //   network_id: "*",
-    //   gas: 4600000
-    // },
     test: {
       network_id: "*",
       provider: ganache.provider()
